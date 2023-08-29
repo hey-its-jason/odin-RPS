d1b3d76b9628f61f9547567f79b61514e81efb62
Jason <heyitsjason11@gmail.com>
2023-08-29 10:56:09 -0400

    Added selections and tested a round

--- index.js
+++ index.js
@@ -5,10 +5,47 @@ const options = ["rock", "paper", "scissor"];  //1. Game choices
 
 function getComputerChoice(){ //2. Computer chooses
     const choice = options[Math.floor(Math.random() * options.length)];
-    console.log(choice);
+    return choice;
 }
-getComputerChoice();
-    //3. Player chooses
+
+function winner(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        return "Tie";
+    }
+    else if(
+        (playerSelection == "rock" && computerSelection == "scissor") ||
+        (playerSelection == "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissor" && computerSelection == "paper")
+    ){
+        return "Player";
+    }
+    else {
+        return "Computer";
+    }
+}
+
+function playRound(playertSelection, computerSelection){
+    const result = winner(playertSelection, computerSelection);
+        if(result == "Tie"){
+            return "DRAW!"
+    }
+        else if(result == "Player"){
+            return `Player wins! ${playerSelection} beats ${computerSelection}`
+    }
+        else if(result == "Computer"){
+            return `Computer wins! ${computerSelection} beats ${playertSelection}`
+    }
+
+}
+
+const playerSelection = "rock";
+const computerSelection = "paper";
+console.log(playRound(playerSelection, computerSelection));
+
+
+
+//getComputerChoice();
+//3. Player chooses
 //4. Randomize choices
 //5. Who 
 //6. Reset game
